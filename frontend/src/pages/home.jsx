b5b22b3cb8a92bad6ad97d87189d84b859d4b2f5
PabloSab <psabogal2005@gmail.com>
2025-03-24 15:35:07 -0400

    Button to Rate page

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,23 +1,28 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Home = () => {
   return (
     <div
-  className="hero min-h-screen"
-  style={{
-    backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
-  }}>
-  <div className="hero-overlay"></div>
-  <div className="hero-content text-neutral-content text-center">
-    <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">Welcome to Rate My Courses!</h1>
-      <p className="mb-5">
-        Our website is meant to be a place where one can share their experiences
-      </p>
-      <button className="btn btn-primary">Get Started</button>
+      className="hero min-h-screen"
+      style={{
+        backgroundImage:
+          "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+      }}
+    >
+      <div className="hero-overlay"></div>
+      <div className="hero-content text-neutral-content text-center">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Welcome to Rate My Courses!</h1>
+          <p className="mb-5">
+            Our website is meant to be a place where one can share their experiences
+          </p>
+          <NavLink to="/rateCourse">
+            <button className="btn btn-primary">Get Started</button>
+          </NavLink>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
   );
 };
 
