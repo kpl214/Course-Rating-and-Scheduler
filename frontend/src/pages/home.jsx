7f5357a4d928007d4c09aaffcf66a4fd3591b645
PabloSab <psabogal2005@gmail.com>
2025-03-24 14:33:28 -0400

    First commit

---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div
+  className="hero min-h-screen"
+  style={{
+    backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+  }}>
+  <div className="hero-overlay"></div>
+  <div className="hero-content text-neutral-content text-center">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-5xl font-bold">Welcome to Rate My Courses!</h1>
+      <p className="mb-5">
+        Our website is meant to be a place where one can share their experiences
+      </p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+  );
+};
+
+export default Home;
