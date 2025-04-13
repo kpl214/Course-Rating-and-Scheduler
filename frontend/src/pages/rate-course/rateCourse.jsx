42a3db52df7dbf547afa9168f5dfe6cf316ec562
salena-till22 <salena.till@ufl.edu>
2025-04-12 22:39:55 -0400

    Created a search bar on the rateCourse page

--- frontend/src/pages/rate-course/rateCourse.jsx
+++ frontend/src/pages/rate-course/rateCourse.jsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import { useRateCourse } from "./useRateCourse.js";
 
 const RateCourse = () => {
-  const [comment, setComment] = useState();
+  const [comment, setComment] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
   const { postComment } = useRateCourse();
   const onCommentChange = (e) => {
     setComment(e.target.value);
@@ -12,7 +13,12 @@ const RateCourse = () => {
   };
 
   return (
-    <div>
+    <div className="flex flex-col items-center gap-4 w-full">
+      <fieldset className="fieldset w-full max-w-3xl">
+        <legend className="fieldset-legend text-lg font-semibold mb-2">Type your course code here:</legend>
+          <input type="text" className="input input-bordered input-lg w-full" placeholder="Type here" />
+            <p className="fieldset-label text-lg font-semibold mb-2">Ex: COP3503</p>
+      </fieldset>
       <textarea
         className="textarea"
         placeholder="Post"
